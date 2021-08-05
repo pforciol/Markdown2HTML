c4bc5f237db4f01ddea1c0162de15fc104805c6b
Pierre Forcioli <2500@holbertonschool.com>
2021-08-05 09:01:40 +0000

    Add all tasks

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# My title
-- Hello
-- Bye
+# Markdown2HTML
 
-* Hello
-* Bye
+## This is a tool to convert `.md` files into `.html` files.
+
+Here is the current features:
+
+-   Headings conversion
+-   Unordered / Ordered lists (with `-` and `*`)
+-   Simple texts
+-   Bold and emphasis texts (with `**` and `__`)
+-   MD5 comversion (with `[[ ]]`)
+-   Case insensitive `c` removal (with `(( ))`)
--- markdown2html.py
+++ markdown2html.py
@@ -1,76 +1,125 @@
 #!/usr/bin/python3
-"""Markdown to HTML module.
+"""markdown2html.py - A quick & dirty Markdown > HTML compiler"""
 
-This module contains a basic script with no utility for now.
-
-"""
-import sys
-import os
+import hashlib
 import re
+from sys import argv
+from sys import exit
 
 if __name__ == "__main__":
 
-    args = sys.argv[1:]
-    args_len = len(args)
+    if (len(argv) < 3):
+        exit("Usage: ./markdown2html.py README.md README.html")
 
-    if args_len < 2:
-        sys.exit("Usage: ./markdown2html.py README.md README.html")
+    if type(argv[1]) is str:
+        try:
+            with open(argv[1], "r") as fp:
+                html = fp.readlines()
+        except IOError:
+            exit("Missing {:s}".format(argv[1]))
 
-    if not os.path.isfile(args[0]):
-        sys.exit("Missing {:s}".format(args[0]))
+    # Flags for multi-line elements
+    ulFlag = False
+    olFlag = False
+    pFlag = False
 
-    with open(args[0], "r") as f:
-        content = f.readlines()
+    # Main loop
+    for i in range(len(html)):
 
-    html = []
-    for line in content:
-        # HEADINGS CONVERSION
-        if line.startswith("#"):
-            level = len(line.split(" ", 1)[0])
-            if level > 6:
-                html.append(line)
-                continue
-            html.append("<h{0}>{1}</h{0}>\n"
-                        .format(level, line.split(" ", 1)[1].strip()))
+        # Headings
+        if html[i].strip().startswith("#"):
+            lvl = html[i].split(" ")[0].count("#")
+            content = html[i].split(" ", 1)[1].strip()
+            if lvl <= 6:
+                html[i] = "<h{0}>{1}</h{0}>\n".format(lvl, content)
 
-        else:
-            html.append(line)
-
-    html_len = len(html)
-
-    # UNORDERED LISTS
-    status = 0
-    for i in range(html_len):
-        if html[i].startswith("-"):
-            if status == 0:
-                html[i] = ("<ul>\n<li>{}</li>\n"
-                           .format(html[i].split(" ", 1)[1].strip()))
+        # Unordered lists
+        if html[i].strip().startswith("- "):
+            content = html[i].split(" ", 1)[1].strip()
+            if not ulFlag:
+                if pFlag:
+                    html[i] = "</p>\n<ul>\n<li>{:s}</li>\n".format(content)
+                else:
+                    html[i] = "<ul>\n<li>{:s}</li>\n".format(content)
             else:
-                html[i] = ("<li>{}</li>\n"
-                           .format(html[i].split(" ", 1)[1].strip()))
+                html[i] = "<li>{:s}</li>\n".format(content)
 
-            status = 1
+            ulFlag = True
 
-            if i + 1 >= html_len or not html[i + 1].startswith("-"):
+            if i + 1 >= len(html) or not html[i + 1].strip().startswith("- "):
                 html[i] += "</ul>\n"
-                status = 0
+                ulFlag = False
 
-    # ORDERED LISTS
-    status = 0
-    for i in range(html_len):
-        if html[i].startswith("*"):
-            if status == 0:
-                html[i] = ("<ol>\n<li>{}</li>\n"
-                           .format(html[i].split(" ", 1)[1].strip()))
+        # Ordered lists
+        if html[i].strip().startswith("* "):
+            content = html[i].split(" ", 1)[1].strip()
+            if not olFlag:
+                if pFlag:
+                    html[i] = "</p>\n<ol>\n<li>{:s}</li>\n".format(content)
+                    pFlag = False
+                else:
+                    html[i] = "<ol>\n<li>{:s}</li>\n".format(content)
             else:
-                html[i] = ("<li>{}</li>\n"
-                           .format(html[i].split(" ", 1)[1].strip()))
+                html[i] = "<li>{:s}</li>\n".format(content)
 
-            status = 1
+            olFlag = True
 
-            if i + 1 >= html_len or not html[i + 1].startswith("*"):
+            if i + 1 >= len(html) or not html[i + 1].strip().startswith("* "):
                 html[i] += "</ol>\n"
-                status = 0
+                olFlag = False
 
-    with open(args[1], "w") as f:
-        f.writelines(html)
+        # Paragraphs
+        if not html[i].strip().startswith("<")\
+                and not re.match(r'\s', html[i]):
+            content = html[i].strip()
+            if not pFlag:
+                html[i] = "<p>\n" + html[i]
+            else:
+                html[i - 1] = html[i - 1].strip() + "<br />\n"
+
+            pFlag = True
+
+            if i + 1 >= len(html) or re.match(r'\s', html[i + 1]):
+                html[i] = html[i].strip() + "\n</p>\n"
+                pFlag = False
+
+        # Bold text
+        if "**" in html[i]:
+            for j in range(int(html[i].count("**") / 2)):
+                boldStart = html[i].index("**")
+                boldStop = html[i].index("**", boldStart + 2)
+                boldText = html[i][boldStart + 2:boldStop]
+                html[i] = html[i].replace(
+                    "**{:s}**".format(boldText), "<b>{:s}</b>".format(boldText))
+
+        # Emphasis
+        if "__" in html[i]:
+            for j in range(int(html[i].count("__") / 2)):
+                emStart = html[i].index("__")
+                emStop = html[i].index("__", emStart + 2)
+                emText = html[i][emStart + 2:emStop]
+                html[i] = html[i].replace("__{:s}__".format(
+                    emText), "<em>{:s}</em>".format(emText))
+
+        # Hash to MD5
+        if "[[" in html[i] and "]]" in html[i]:
+            for j in range(int(html[i].count("[["))):
+                md5Start = html[i].index("[[")
+                md5Stop = html[i].index("]]")
+                md5Text = html[i][md5Start + 2:md5Stop]
+                md5Hash = hashlib.md5(md5Text.encode())
+                html[i] = html[i].replace("[[{:s}]]".format(md5Text),
+                                          str(md5Hash.hexdigest()))
+
+        # Strip the letter c (lower + upper)
+        if "((" in html[i] and "))" in html[i]:
+            for j in range(int(html[i].count("(("))):
+                cStart = html[i].index("((")
+                cStop = html[i].index("))")
+                cText = html[i][cStart + 2:cStop]
+                cLessText = re.sub(r"[cC]", "", cText)
+                html[i] = html[i].replace("(({:s}))".format(cText), cLessText)
+
+    # Write final result to html file
+    with open(argv[2], "w") as output:
+        output.writelines(html)
